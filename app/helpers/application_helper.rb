2cc92c4a53e0ed1f66da60d8ba60eb1943505e2b
opportunato <ryu.gordeyev@gmail.com>
2015-10-03 20:20:03 +0300

    Added video iframe to post page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def post_image(post, is_featured)
     post.preview_image.url(is_featured ? :large : :normal)
   end
+
+  def post_video(post)
+    link = case post.video_type
+    when :youtube
+      "https://www.youtube.com/embed/#{post.video_id}"
+    when :vimeo
+      "https://player.vimeo.com/video/#{post.video_id}"
+    end
+
+    content_tag :iframe, nil, src: link, frameborder: "0", allowfullscreen: true
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,18 @@ class Post < ActiveRecord::Base
   friendly_id :author, use: :slugged
 
   validates_presence_of :title, :author, :body, :video_url, :link, :preview_image
+
+  def video_type
+    if (video_url.include? "youtube.com") then return :youtube end
+    if (video_url.include? "vimeo.com") then return :vimeo end
+  end
+
+  def video_id
+    case video_type
+    when :youtube
+      video_url.match(/youtube.com\/watch\?v=(\w+)/).try(:[], 1)
+    when :vimeo
+      video_url.match(/vimeo.com\/(\d+)/).try(:[], 1)
+    end
+  end
 end
