c70e6aa33ef161f005f3b8cf4f56caccb873b0a9
panddr <panddr@gmail.com>
2015-12-04 14:45:54 +0300

    updated design

--- app/uploaders/preview_image_uploader.rb
+++ app/uploaders/preview_image_uploader.rb
@@ -1,11 +1,11 @@
 class PreviewImageUploader < BaseUploader
   version :normal do
-    process resize_to_fill: [922, 620]
+    process resize_to_fill: [1000, 500]
     process convert: 'jpg'
   end
 
   version :large do
-    process resize_to_fill: [1600, 1000]
+    process resize_to_fill: [1600, 800]
     process convert: 'jpg'
   end
 end
