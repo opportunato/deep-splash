6764d56c60632c6b20f335de6d6b88d2305ac6d1
opportunato <ryu.gordeyev@gmail.com>
2015-12-08 19:50:41 +0300

    returned popup script back to manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,22 @@
 //= require jquery
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+  var $aboutLink = $('.about-link'),
+      $body = $('body'),
+      $aboutPopup = $('.about-popup'),
+      $overlay = $('.overlay');
+
+  $aboutLink.on('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    $body.addClass('no-scroll');
+    $aboutPopup.addClass('active');
+  });
+
+  $overlay.on('click', function(event) {
+    $body.removeClass('no-scroll');
+    $aboutPopup.removeClass('active');
+  });
+});
